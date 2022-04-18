3b1c278f70af21561b39ac3d1237586a9cea92af
Thim Lohse <thim.lohse@caesari.com>
2022-04-18 17:40:11 +0200

    new color gradient!

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,9 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 const gradients = [
   "from-indigo-300 via-purple-300 to-pink-200",
   "from-indigo-100 via-pink-100 to-pink-50",
+  "from-champagne via-pale-pink to-pale-pink",
 ];
 export default function Document() {
-  const baseStyling = `min-h-screen bg-gradient-to-r ${gradients[1]}`;
+  const baseStyling = `min-h-screen bg-gradient-to-br ${gradients[2]}`;
   return (
     <Html>
       <Head />
